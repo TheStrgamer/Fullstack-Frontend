fc88c3c8468595d068e7544aff9e7be489f9e9a5
Konrad <konradseime@gmail.com>
2025-04-01 11:08:30 +0200

    Fix: removed proojectname from url

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import Login from '../views/Login.vue'
 import Register from '@/views/Register.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
