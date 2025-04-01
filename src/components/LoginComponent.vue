a3afea4fe5122b03224b6e7f648ba74f6b3a36c8
jonasreiher <jonareih@stud.ntnu.no>
2025-04-01 14:50:39 +0200

    feat linked login page to backend and authenticates and redirects user to main page correctly

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -10,6 +10,8 @@
 
 
 <script lang="ts">
+    import router from '@/router';
+import axios from 'axios';
     export default {
         name: 'RegisterComponent',
         data() {
@@ -19,10 +21,28 @@
             }
         },
         methods: {
-            onLogin() {
-                console.log('login with email:', this.email, ' password:', this.password)
+            async onLogin() {
+                console.log('login with email:', this.email, ' password:', this.password);
+                try {
+                    const response = await axios.post('http://localhost:8080/api/users/login', {
+                        email: this.email,
+                        password: this.password
+                    });
+
+                    if(!response) {
+                        console.log(response);
+                        throw new Error("Login Failed!");
+                    }
+
+                    console.log('Login successful:', response.data);
+                    router.push('/');
+                    
+                } catch (error) {
+                    console.error('Login error:', error);
+                }
+                
             },
-        },
+        }
     }
 </script>
   
