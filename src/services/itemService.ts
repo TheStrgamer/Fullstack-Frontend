ff00110b0de02bc940e37c3614f5de3621f93fb3
Eskild Smestu <esmestu@icloud.com>
2025-04-02 11:17:13 +0200

    Fix: Fetch items by id

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -1,5 +1,5 @@
 export async function fetchItemFromAPI(itemId: string) {
-  const response = await fetch(`api-url/${itemId}`, {
+  const response = await fetch(`http://localhost:8080/api/listings/id/${itemId}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
