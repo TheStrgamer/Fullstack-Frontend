e1b9d7786fc7d5dff23faf8b7263f2eca76f231f
Konrad <konradseime@gmail.com>
2025-04-02 13:52:08 +0200

    Added validation for login

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,8 +1,14 @@
 <template>
     <form @submit.prevent="onLogin" class = "input-form-flex">
 
-        <input v-model="email" type="text" id="email" name="email" placeholder="email" />
-        <input v-model="password" type="password" id="password" name="password" placeholder="password" />
+        <div>
+            <input v-model="email" type="text" id="email" name="email" placeholder="email" />
+            <p class="error-message">{{ emailErrorMessage }}</p>
+        </div>        
+        <div>
+            <input v-model="password" type="password" id="password" name="password" placeholder="password" />
+            <p class="error-message">{{ passwordErrorMessage }}</p>
+        </div>
         <button type="submit" >Login</button>
         <p>Don't have an account? <router-link to="/register">Register</router-link></p>
     </form>
@@ -11,17 +17,69 @@
 
 <script lang="ts">
     import router from '@/router';
-import axios from 'axios';
+    import axios from 'axios';
     export default {
         name: 'RegisterComponent',
         data() {
             return {
                 email: '',
                 password: '',
+
+                emailErrorMessage: '',
+                passwordErrorMessage: '',
             }
         },
         methods: {
+            verifyEmail() {
+                const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                return emailPattern.test(this.email);
+            },
+            verifyPassword() {
+                return this.password.length >= 3;
+            },
+            verifyField(field: string, id: string) {
+                let fieldValid = false;
+                switch (field) {
+                    case 'email':
+                        fieldValid = this.verifyEmail();
+                        if (!fieldValid) {
+                            this.emailErrorMessage = 'Please enter a valid email';
+                        } else {
+                            this.emailErrorMessage = '';
+                        }
+                        break;
+                    case 'password':
+                        fieldValid = this.verifyPassword();
+                        if (!fieldValid) {
+                            this.passwordErrorMessage = 'Password must be at least 3 characters long';
+                        } else {
+                            this.passwordErrorMessage = '';
+                        }
+                        break;
+                    default:
+                        break;
+                    
+                }
+                const element = document.getElementById(id);
+                if (element) {
+                    if (fieldValid) {
+                        element.classList.remove('invalid-field');
+                    } else {
+                        element.classList.add('invalid-field');
+                    }
+                }
+                return fieldValid;
+            },
+            verifyForm() {
+                let isValid = true;
+                isValid = this.verifyField('email', 'email') && isValid;
+                isValid = this.verifyField('password', 'password') && isValid;
+            return isValid;
+            },
             async onLogin() {
+                if (!this.verifyForm()) {
+                    return;
+                }
                 console.log('login with email:', this.email, ' password:', this.password);
                 try {
                     const response = await axios.post('http://localhost:8080/api/users/login', {
@@ -35,6 +93,7 @@ import axios from 'axios';
                     }
 
                     console.log('Login successful:', response.data);
+
                     router.push('/');
                     
                 } catch (error) {
