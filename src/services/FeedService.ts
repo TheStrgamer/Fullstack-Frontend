5db95d75458b1d714a4c7c64c88c2aef7e17f9b5
Eskild Smestu <esmestu@icloud.com>
2025-04-03 11:25:46 +0200

    Fix: Remove token from fetch items

--- src/services/FeedService.ts
+++ src/services/FeedService.ts
@@ -1,22 +1,9 @@
-import { useUserStore } from '../stores/UserStore';
-
 // Fetch recommended items from the API
 export const fetchRecommendedItems = async (count: number) => {
-  const userStore = useUserStore();
-  let token = userStore.jwtToken;
-  if (!token) {
-    console.error('No JWT token found in store');
-    return null;
-  }
-  if (userStore.isTokenExpired()) {
-    console.warn('JWT token is expired');
-    return null;
-  }
   const response = await fetch(`http://localhost:8080/api/listings/all`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
     },
   });
   console.log(response);
