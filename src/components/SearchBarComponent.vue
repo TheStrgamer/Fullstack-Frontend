39c412f1282115d9e025ff7043d9b9e57a3f8ba4
zvindel <erlendez@stud.ntnu.no>
2025-04-03 11:06:49 +0200

    feat: added search bar and category list

---
+++ src/components/SearchBarComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="search-bar">
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Søk etter produkter..."
+        class="search-input"
+      />
+      <button class="search-button" @click="onSearch">Søk</button>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue'
+  
+  const searchQuery = ref('')
+  
+  function onSearch() {
+    // TODO: emit event or use store to trigger filtering
+    console.log('Søker etter:', searchQuery.value)
+  }
+  </script>
+
+  <style>
+  @import '../assets/SearchBarComponent.css';
+  </style>
+  
