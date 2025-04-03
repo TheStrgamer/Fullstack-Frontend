316e6b3d64486898a7092902e3904a96dd95f646
Eskild Smestu <esmestu@icloud.com>
2025-04-03 10:34:07 +0200

    Feat: Implement support for jwt token

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,48 +1,39 @@
 <template>
-    <form @submit.prevent="onLogin" class = "input-form-flex">
-
-        <input v-model="email" type="text" id="email" name="email" placeholder="email" />
-        <input v-model="password" type="password" id="password" name="password" placeholder="password" />
-        <button type="submit" >Login</button>
-        <p>Don't have an account? <router-link to="/register">Register</router-link></p>
-    </form>
-  </template>
-
+  <form @submit.prevent="onLogin" class="input-form-flex">
+    <input v-model="email" type="text" id="email" name="email" placeholder="email" />
+    <input v-model="password" type="password" id="password" name="password" placeholder="password" />
+    <button type="submit">Login</button>
+    <p>Don't have an account? <router-link to="/register">Register</router-link></p>
+  </form>
+</template>
 
 <script lang="ts">
-    import router from '@/router';
-import axios from 'axios';
-    export default {
-        name: 'RegisterComponent',
-        data() {
-            return {
-                email: '',
-                password: '',
-            }
-        },
-        methods: {
-            async onLogin() {
-                console.log('login with email:', this.email, ' password:', this.password);
-                try {
-                    const response = await axios.post('http://localhost:8080/api/users/login', {
-                        email: this.email,
-                        password: this.password
-                    });
+import router from '@/router';
+import { useUserStore } from '@/stores/UserStore';
+import { defineComponent, ref, onMounted } from 'vue';
 
-                    if(!response) {
-                        console.log(response);
-                        throw new Error("Login Failed!");
-                    }
+export default defineComponent({
+  name: 'RegisterComponent',
+  setup() {
+    const userStore = useUserStore();
+    const email = ref('');
+    const password = ref('');
 
-                    console.log('Login successful:', response.data);
-                    router.push('/');
-                    
-                } catch (error) {
-                    console.error('Login error:', error);
-                }
-                
-            },
-        }
-    }
+    const onLogin = async () => {
+      console.log('login with email:', email.value, ' password:', password.value);
+      try {
+        await userStore.getTokenAndSaveInStore(email.value, password.value);
+        await router.push("/");
+      } catch (error) {
+        console.error('Login error:', error);
+      }
+    };
+
+    return {
+      email,
+      password,
+      onLogin,
+    };
+  },
+});
 </script>
-  
---
+++ src/services/authService.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export async function getJwtToken(email: string, password: string): Promise<{ token: string, expiresAt: number }> {
+  try {
+    const response = await axios.post('http://localhost:8080/api/users/login', {
+      email: email,
+      password: password
+    });
+
+    if (!response || !response.data) {
+      throw new Error("Login Failed!");
+    }
+
+    console.log('Login successful:', response.data);
+
+    return response.data;
+
+  } catch (error) {
+    console.error('Login error:', error);
+    throw error;
+  }
+}
