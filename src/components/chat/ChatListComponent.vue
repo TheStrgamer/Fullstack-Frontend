bff87c09e76933e8c3e08729c64d9f1c3868bd4e
Konrad <konradseime@gmail.com>
2025-04-05 10:54:45 +0200

    Feat: made simple chat list components

---
+++ src/components/chat/ChatListComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="chat-list">
+        <h2 class="chat-list-title">Chat</h2>
+        <div class="chat-list-items">
+          <ChatListItem
+            v-for="chat in chats"
+            :key="chat.id"
+            :user="chat"
+            :lastMessage="chat.lastMessage"
+            :timestamp="chat.timestamp"
+          />
+        </div>
+    </div>    
+</template>
+  
+  <script lang="ts">
+  import ChatListItem from '@/components/chat/ChatListItem.vue';
+  
+  interface Chat {
+    id: number;
+    name: string;
+    avatar: string;
+    lastMessage: string;
+    timestamp: string;
+  }
+  
+  export default {
+    name: 'ChatListComponent',
+    components: {
+      ChatListItem
+    },
+    props: {
+      chats: {
+        type: Array as () => Chat[],
+        required: true
+      }
+    }
+  }
+  </script>
+  
+<style scoped>
+    @import '@/assets/chat.css';
+</style>
+
---
+++ src/components/chat/ChatListItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="chat-list-item">
+        <div class="chat-list-item-content">
+            <img :src="user.avatar" alt="User Avatar" class="avatar" v-if="user.avatar!=''" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSieyaZb-hSOtWnc6wha3QQlMLL8_cfvr2WIQ&s" alt="Default Avatar" class="avatar" v-else />
+            <div class="chat-list-item-text">
+                <h3>{{ user.name }}</h3>
+                <p>{{ lastMessage }}</p>
+            </div>
+        </div>
+        <span class="timestamp">{{ timestamp }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'ChatListItem',
+    props: {
+      user: {
+        type: Object,
+        required: true,
+      },
+      lastMessage: {
+        type: String,
+        default: '',
+      },
+      timestamp: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+
+</script>
