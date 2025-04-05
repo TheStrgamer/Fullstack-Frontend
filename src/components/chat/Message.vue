445ac1f00335ce612eef10ebd979c66d3de78596
Konrad <konradseime@gmail.com>
2025-04-05 10:55:01 +0200

    Feat: added simple message list component

---
+++ src/components/chat/Message.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="message-container" :class="{ 'sent-by-me': user.sentByMe }">
+    <span class="timestamp">{{ timestamp }}</span>
+    <div class="message">
+        <div class="message-list-item-content">
+            <img v-if="user.avatar!=''" :src="user.avatar" alt="User Avatar" class="avatar" />
+            <img  v-else src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSieyaZb-hSOtWnc6wha3QQlMLL8_cfvr2WIQ&s" alt="Default Avatar" class="avatar"/>
+            <div class="message-list-item-text">
+                <h3>{{ user.senderName }}</h3>
+                <p>{{ message }}</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'ChatMessage',
+    props: {
+      user: {
+        type: Object,
+        required: true,
+      },
+      message: {
+        type: String,
+        default: '',
+      },
+      timestamp: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+
+</script>
---
+++ src/components/chat/MessagesListComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="messages-list">
+        <h2 class="message-list-title">{{ name }}</h2>
+        <div class="message-list-items">
+          <ChatMessage
+            v-for="message in messages"
+            :key="message.id"
+            :user="message"
+            :message="message.message"
+            :timestamp="message.timestamp"
+          />
+        </div>
+        <div class="message-list-footer">
+          <input type="text" placeholder="Type a message..." class="message-input" />
+          <button class="send-button">Send</button>
+        </div>
+    </div>    
+</template>
+  
+<script lang="ts">  
+  import ChatMessage from '@/components/chat/Message.vue';
+  interface Message {
+    id: number;
+    sentByMe: boolean;
+    senderName: string;
+    avatar: string;
+    message: string;
+    timestamp: string;
+  }
+  
+  export default {
+    name: 'ChatMessagesComponent',
+    components: {
+      ChatMessage
+    },
+    props: {
+      messages: {
+        type: Array as () => Message[],
+        required: true
+      },
+      name: {
+        type: String,
+        default: 'Chat'
+      }
+    },
+  }
+</script>
+  
+<style scoped>
+    @import '@/assets/chat.css';
+    
+</style>
+
